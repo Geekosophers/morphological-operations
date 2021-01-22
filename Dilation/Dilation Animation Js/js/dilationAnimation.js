537df71d191520b38ecd8f3d7a3a16067a1bf6cf
Anshul Sachdev <anshulsachdev@Anshuls-MacBook-Pro.local>
2021-01-22 23:34:26 +0530

    hosting on heroku

--- Dilation/Dilation Animation Js/js/dilationAnimation.js
+++ Dilation/Dilation Animation Js/js/dilationAnimation.js
@@ -101,7 +101,7 @@ var updateKernelPosition = function(){
 
 
 var scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xFFFFFF );
+scene.background = new THREE.Color( 0x4b5399 );
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
