ab72e0d887efad4598ff992adfbd3c9efc1be14f
Anshul Sachdev <anshulsachdev@Anshuls-MacBook-Pro.local>
2021-01-26 19:26:06 +0530

    small changes

--- Dilation/dilation.py
+++ Dilation/dilation.py
@@ -1,17 +1,48 @@
 import numpy as np
 import cv2
 
-filepath = 'assets/dilation7x7sample2.jpg'
+filepath = 'Dilation/assets/dilation7x7sample2.jpg'
 img = cv2.imread(filepath,0)
 
-kernel = arr = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], np.uint8)
-# Kernel
-# 0 1 0
-# 1 1 1
-# 0 1 0
+# img = np.array([[0,   0,   0, 0,   0],
+#                 [0,   0, 255, 0, 255],
+#                 [0,   0,   0, 0, 255],
+#                 [0,   0, 255, 0,   0],
+#                 [0,   0, 255, 0, 255],
+#                 [255, 0,   0, 0,   0]], np.uint8)
+
+kernel = np.array([[0, 1, 0, 0, 0],
+                   [1, 0, 1, 0, 0],
+                   [0, 1, 1, 0, 0],
+                   [0, 0, 0, 0, 0],
+                   [1, 1, 1, 1, 1]], np.uint8)
 
 dilation = cv2.dilate(img,kernel,iterations = 1)
-# The code automatically uses the padding of '0' while dilating the image
 
-outpath = 'assets/resultDilation.jpg'
+outpath = 'Dilation/assets/resultDilation.jpg'
 cv2.imwrite(outpath,dilation)
+
+##################################################################
+
+# The code automatically creates a padding of '0' around the image
+
+# Input Image (3X5): 
+# 1 0 1 0 0
+# 1 1 0 0 1 
+# 0 0 1 1 0 
+
+# Padded Image for 3X3 kernel:
+# 0 0 0 0 0 0 0
+# 0 1 0 1 0 0 0
+# 0 1 1 0 0 1 0
+# 0 0 0 1 1 0 0
+# 0 0 0 0 0 0 0
+
+# Padded Image for 5X5 kernel:
+# 0 0 0 0 0 0 0 0 0
+# 0 0 1 0 1 0 0 0 0
+# 0 0 1 1 0 0 1 0 0
+# 0 0 0 0 1 1 0 0 0
+# 0 0 0 0 0 0 0 0 0
+
+##################################################################
